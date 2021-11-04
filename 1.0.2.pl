4b79faa2cd8c64d6b21af14cde731a8b7dad4654
chicagoist <le.legioner@gmail.com>
2021-11-04 18:55:06 +0200

    С поомщью регулярного выражения.

--- 1.0.2.pl
+++ 1.0.2.pl
@@ -8,10 +8,12 @@ sub search_a_word {
     $_ =
 "This is a dog. It is big and black.  Odograph is instrument for recording courses steered by a vessel with the distances or lengths of time run on each.";
 
-    $_ =~ s/dog*/DOG/g;    # через регулярные
+    print $_ . "\n\n";    # эталонная строка
 
-    print $_ . "\n";       # изменённая строка
+    $_ =~ s/dog*/DOG/g; # через регулярные
+
+    print $_ . "\n";    # изменённая строка
 
 }
 
-&search_a_word;            # вызов функции
+&search_a_word;         # вызов функции
